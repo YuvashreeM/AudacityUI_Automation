3246e06420db93c9d3a918e852869067b74b35ac
YuvashreeM <yuvashree.m@rently.com>
2025-06-15 14:41:35 +0530

    refactor the test file

--- tests/search-input.spec.ts
+++ tests/search-input.spec.ts
@@ -1,7 +1,8 @@
 import {test, expect} from "../base/pomFixture";
 import pageTitle from "../testData/pageTitle.json"
-import {step} from 'allure-js-commons';
+import {description, step} from 'allure-js-commons';
 import * as catalogData from "../testData/catalogData.json";
+import { describe } from "node:test";
 
 
 test.beforeEach(async ({ page, baseURL }) => {
@@ -20,19 +21,19 @@ test.beforeEach(async ({ page, baseURL }) => {
     }
 })
 
-test ('@smoke Search with valid data',  async ({ headerPage, catalogPage, apiClient }) => {
+
+test ('@smoke Catalog Search Functionality With Valid Data',  async ({ headerPage, catalogPage, apiClient }) => {
+    description(`Search with valid data: ${catalogData.validSearchText}`); // Add a description for the test    
     await headerPage.catalog(); // Navigate to the catalog page
-    await catalogPage.search(catalogData.validSearchText); // Perform a search with invalid data
+    await catalogPage.search(catalogData.validSearchText); // Perform a search with valid data
     await catalogPage.sortByRating(catalogData.ratingFilter); // Sort by rating
     await catalogPage.skillFilter(catalogData.skillSearchInput); // Apply the skill filter
     await catalogPage.levelFilter(); // Apply the level filter
     await catalogPage.searchResults(catalogData.skillSearchInput, catalogData.skillValue, catalogData.levelFilter, apiClient); // Verify the search results are displayed
-
 })
 
-
-
 test ('@smoke Search with invalid data',  async ({ page, headerPage, catalogPage, apiClient }) => {
+    description(`Search with invalid data: ${catalogData.invalidSearchText}`);
     await headerPage.catalog(); // Navigate to the catalog page
     await catalogPage.search(catalogData.invalidSearchText); // Perform a search with invalid data
     await catalogPage.searchResults(catalogData.invalidSearchText,null,null, apiClient); // Verify the search results are displayed
